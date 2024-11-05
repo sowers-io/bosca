ef5b177c79a7abbbd3af15299b44b29699c4b0b4
Kyle Brock <kyle@sowers.io>
2024-11-05 08:57:03 -0500

    upload objects

--- workspace/analytics/src/writers/files.rs
+++ workspace/analytics/src/writers/files.rs
@@ -6,9 +6,15 @@ use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::Relaxed;
 use std::time::Duration;
+use bytes::{Buf, BytesMut};
 use chrono::Utc;
 use log::{error, info};
+use object_store::aws::AmazonS3Builder;
+use object_store::{MultipartUpload, ObjectStore};
+use object_store::path::Path;
+use tokio::io::AsyncReadExt;
 use tokio::task;
+use ulid::Ulid;
 use crate::writers::arrow::copy::copy_to_parquet;
 use crate::writers::arrow::parquet::writer::new_arrow_writer;
 use crate::writers::arrow::schema::SchemaDefinition;
@@ -60,90 +66,152 @@ pub async fn watch_files(writer: Arc<EventsWriter>, schema: Arc<SchemaDefinition
                 tokio::fs::create_dir_all(&config.pending_objects_dir).await.unwrap();
             }
         }
-        if let Ok(mut read) = tokio::fs::read_dir(&config.temp_dir).await {
-            let mut files = Vec::new();
-            let mut file_sizes = 0u64;
-            while let Ok(Some(entry)) = read.next_entry().await {
-                if let Ok(file_type) = entry.file_type().await {
-                    if file_type.is_file() {
-                        if let Ok(metadata) = entry.metadata().await {
-                            file_sizes += metadata.st_size();
-                        }
-                        files.push(entry);
-                    }
-                }
-            }
-            if file_sizes >= config.max_file_size {
-                writer.recycle().await;
-                let parquet_file = format!("{}/batch-{}.parquet", &config.batches_dir, Utc::now().timestamp_millis());
-                let finished_parquet_file = format!("{}/batch-{}.parquet", &config.pending_objects_dir, Utc::now().timestamp_millis());
-                let writer = Arc::new(Mutex::new(new_arrow_writer(Arc::clone(&schema), &parquet_file, 10000).unwrap()));
-                let mut success = true;
-                for file in &files {
-                    if let Ok(file_name) = file.file_name().into_string() {
-                        if file_name.starts_with("events-") && file_name.ends_with(".json") {
-                            info!("adding json file to parquet: {}", file_name);
-                            let spawn_file = format!("{}/{}", &config.temp_dir, file_name);
-                            let spawn_writer = Arc::clone(&writer);
-                            let spawn_writer_schema = Arc::clone(&schema);
-                            success = task::spawn_blocking(move || {
-                                match File::open(spawn_file) {
-                                    Ok(file) => {
-                                        if let Err(err) = copy_to_parquet(file, spawn_writer_schema, spawn_writer) {
-                                            error!("error copying file to parquet: {:?}", err);
-                                        }
-                                    }
-                                    Err(e) => {
-                                        error!("error opening file for parquet copy: {:?}", e);
-                                        return false;
-                                    }
-                                }
-                                true
-                            }).await.unwrap_or_else(|e| {
-                                error!("error copying file: {:?}", e);
-                                false
-                            });
-                            if !success {
-                                let _ = writer.lock().unwrap().finish();
-                                break;
-                            }
-                        }
-                    }
-                }
-                if success {
-                    let mut writer = writer.lock().unwrap();
-                    if let Err(e) = writer.flush() {
-                        error!("error flushing parquet records: {:?}", e);
-                        success = false;
-                    } else if let Err(e) = writer.finish() {
-                        error!("error finishing parquet: {:?}", e);
-                        success = false;
-                    }
-                }
-                if success {
-                    for file in files {
-                        if let Ok(file_name) = file.file_name().into_string() {
-                            if file_name.starts_with("events-") && file_name.ends_with(".json") {
-                                if let Err(err) = tokio::fs::remove_file(format!("{}/{}", &config.temp_dir, file_name)).await {
-                                    error!("error deleting file: {:?}", err);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if let Err(err) = tokio::fs::rename(parquet_file, finished_parquet_file).await {
-                        error!("error deleting file: {:?}", err);
-                        break;
-                    }
-                } else if let Err(err) = tokio::fs::remove_file(parquet_file).await {
-                    error!("error deleting file: {:?}", err);
-                    break;
-                }
-            }
-        } else {
-            error!("error processing files");
+        if let Err(err) = watch_json(&writer, &schema, &config).await {
+            error!("error watching json: {:?}", err);
+        }
+        if let Err(err) = watch_objects(&config).await {
+            error!("error watching json: {:?}", err);
         }
         watching.store(false, Relaxed);
         tokio::time::sleep(Duration::from_secs(15)).await;
     }
+}
+
+async fn watch_objects(config: &Config) -> Result<(), Box<dyn Error>> {
+    if let Ok(mut read) = tokio::fs::read_dir(&config.temp_dir).await {
+        // TODO: make this more generic for multiple object stores
+        let s3 = AmazonS3Builder::from_env().build().unwrap();
+        while let Ok(Some(entry)) = read.next_entry().await {
+            if let Ok(file_type) = entry.file_type().await {
+                if file_type.is_file() {
+                    if let Ok(file_name) = entry.file_name().into_string() {
+                        if file_name.ends_with(".parquet") {
+                            let metadata = entry.metadata().await?;
+                            let created = metadata.created()?;
+                            let utc = time::OffsetDateTime::UNIX_EPOCH
+                                + time::Duration::try_from(created.duration_since(std::time::UNIX_EPOCH).unwrap()).unwrap();
+                            let local = utc.to_offset(time::UtcOffset::local_offset_at(utc).unwrap());
+                            let path = Path::parse(format!(
+                                "analytics/{}/{}/{}/events-{}.parquet",
+                                local.year(), local.month(), local.day(),
+                                Ulid::new().to_string(),
+                            ))?;
+                            let mut upload = s3.put_multipart(&path).await?;
+                            let mut buf = BytesMut::with_capacity(5242880);
+                            let mut file = tokio::fs::File::open(entry.file_name()).await?;
+                            let len = file.metadata().await?.len();
+                            let mut offset = 0;
+                            while offset < len {
+                                let chunk_len = file.read_buf(&mut buf).await?;
+                                let buf_len = buf.len();
+                                if buf_len >= 5242880 {
+                                    let copy = buf.copy_to_bytes(buf_len);
+                                    buf.clear();
+                                    upload
+                                        .put_part(copy.into())
+                                        .await?;
+                                }
+                                offset += chunk_len as u64;
+                            }
+                            if !buf.is_empty() {
+                                let copy = buf.copy_to_bytes(buf.len());
+                                buf.clear();
+                                upload
+                                    .put_part(copy.into())
+                                    .await?;
+                            }
+                            upload.complete().await?;
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        return Err("error processing object files".to_string().into());
+    }
+    Ok(())
+}
+
+async fn watch_json(writer: &Arc<EventsWriter>, schema: &Arc<SchemaDefinition>, config: &Config) -> Result<(), Box<dyn Error>> {
+    if let Ok(mut read) = tokio::fs::read_dir(&config.temp_dir).await {
+        let mut files = Vec::new();
+        let mut file_sizes = 0u64;
+        while let Ok(Some(entry)) = read.next_entry().await {
+            if let Ok(file_type) = entry.file_type().await {
+                if file_type.is_file() {
+                    if let Ok(metadata) = entry.metadata().await {
+                        file_sizes += metadata.st_size();
+                    }
+                    files.push(entry);
+                }
+            }
+        }
+        if file_sizes >= config.max_file_size {
+            writer.recycle().await;
+            let parquet_file = format!("{}/batch-{}.parquet", &config.batches_dir, Utc::now().timestamp_millis());
+            let finished_parquet_file = format!("{}/batch-{}.parquet", &config.pending_objects_dir, Utc::now().timestamp_millis());
+            let writer = Arc::new(Mutex::new(new_arrow_writer(Arc::clone(&schema), &parquet_file, 10000).unwrap()));
+            let mut success = true;
+            for file in &files {
+                if let Ok(file_name) = file.file_name().into_string() {
+                    if file_name.starts_with("events-") && file_name.ends_with(".json") {
+                        info!("adding json file to parquet: {}", file_name);
+                        let spawn_file = format!("{}/{}", &config.temp_dir, file_name);
+                        let spawn_writer = Arc::clone(&writer);
+                        let spawn_writer_schema = Arc::clone(&schema);
+                        success = task::spawn_blocking(move || {
+                            match File::open(spawn_file) {
+                                Ok(file) => {
+                                    if let Err(err) = copy_to_parquet(file, spawn_writer_schema, spawn_writer) {
+                                        error!("error copying file to parquet: {:?}", err);
+                                    }
+                                }
+                                Err(e) => {
+                                    error!("error opening file for parquet copy: {:?}", e);
+                                    return false;
+                                }
+                            }
+                            true
+                        }).await.unwrap_or_else(|e| {
+                            error!("error copying file: {:?}", e);
+                            false
+                        });
+                        if !success {
+                            let _ = writer.lock().unwrap().finish();
+                            break;
+                        }
+                    }
+                }
+            }
+            if success {
+                let mut writer = writer.lock().unwrap();
+                if let Err(e) = writer.flush() {
+                    error!("error flushing parquet records: {:?}", e);
+                    success = false;
+                } else if let Err(e) = writer.finish() {
+                    error!("error finishing parquet: {:?}", e);
+                    success = false;
+                }
+            }
+            if success {
+                for file in files {
+                    if let Ok(file_name) = file.file_name().into_string() {
+                        if file_name.starts_with("events-") && file_name.ends_with(".json") {
+                            if let Err(err) = tokio::fs::remove_file(format!("{}/{}", &config.temp_dir, file_name)).await {
+                                return Err(format!("error deleting file: {:?}", err).into());
+                            }
+                        }
+                    }
+                }
+                if let Err(err) = tokio::fs::rename(parquet_file, finished_parquet_file).await {
+                    return Err(format!("error deleting file: {:?}", err).into());
+                }
+            } else if let Err(err) = tokio::fs::remove_file(parquet_file).await {
+                return Err(format!("error deleting file: {:?}", err).into())
+            }
+        }
+    } else {
+        return Err("error processing files".to_string().into());
+    }
+    Ok(())
 }
