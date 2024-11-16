7d3423fe5fee2d46d680093afb443120c70b2c40
Kyle Brock <kyle@sowers.io>
2024-11-15 20:45:43 -0600

    lint

--- workspace/admin/src/app/metadata/[id]/traits.tsx
+++ workspace/admin/src/app/metadata/[id]/traits.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { Label } from '@/components/ui/label'
 import React from 'react'
 
 interface TraitsProps extends React.HTMLAttributes<HTMLDivElement> {
