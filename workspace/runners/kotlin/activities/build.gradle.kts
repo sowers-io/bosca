d8506f6e243f33afc7ff005a058ecbd5e4d78058
Kyle Brock <kyle@sowers.io>
2025-03-17 12:25:28 -0500

    add -community to the js impl for graal

--- workspace/runners/kotlin/activities/build.gradle.kts
+++ workspace/runners/kotlin/activities/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.10.1")
     implementation("com.dashjoin:jsonata:0.9.8")
     implementation("com.fleeksoft.ksoup:ksoup:0.2.2")
-    implementation("org.graalvm.polyglot:js:24.1.2")
+    implementation("org.graalvm.polyglot:js-community:24.1.2")
     implementation("org.graalvm.polyglot:polyglot:24.1.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.3")
 
