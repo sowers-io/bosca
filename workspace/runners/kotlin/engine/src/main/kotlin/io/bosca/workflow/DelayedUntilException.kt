01f95a1b501ea0b78c58678d74f16123084b1ed2
Kyle Brock <kyle@sowers.io>
2025-03-18 09:55:06 -0500

    adding graal configs for meilisearch

--- workspace/runners/kotlin/engine/src/main/kotlin/io/bosca/workflow/DelayedUntilException.kt
+++ workspace/runners/kotlin/engine/src/main/kotlin/io/bosca/workflow/DelayedUntilException.kt
@@ -3,4 +3,6 @@ package io.bosca.workflow
 import java.time.ZonedDateTime
 
 class DelayedUntilException(val delayedUntil: ZonedDateTime) : Exception() {
+
+    override fun toString() = delayedUntil.toString()
 }
