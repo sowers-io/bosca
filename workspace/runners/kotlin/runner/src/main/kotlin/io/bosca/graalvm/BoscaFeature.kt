a02126056d7f2faf206572cf8f58af3a90519e6d
Kyle Brock <kyle@sowers.io>
2025-03-26 07:49:05 -0500

    tweaking classes

--- workspace/runners/kotlin/runner/src/main/kotlin/io/bosca/graalvm/BoscaFeature.kt
+++ workspace/runners/kotlin/runner/src/main/kotlin/io/bosca/graalvm/BoscaFeature.kt
@@ -96,6 +96,7 @@ class BoscaFeature : Feature {
         register("dev.ai4j.openai4j.shared.Usage", access, recursive = true)
         register("dev.ai4j.openai4j.shared.PromptTokensDetails", access, recursive = true)
         register("dev.ai4j.openai4j.chat.ChatCompletionChoice", access, recursive = true)
+        register("dev.ai4j.openai4j.chat.ToolCall", access, recursive = true)
 
 
         // Register security classes
