d9c1990a98f84890ac40a398c1535953384bc8f4
Kyle Brock <kyle@sowers.io>
2025-03-31 15:52:23 -0500

    add youtube via id

--- workspace/web/administration/components/content/Media.vue
+++ workspace/web/administration/components/content/Media.vue
@@ -13,6 +13,7 @@ import {
   PaginationPrev,
 } from '@/components/ui/pagination'
 import type { Reactive } from 'vue'
+import type { MetadataInput } from '~/lib/graphql/graphql.ts'
 
 const props = defineProps<{
   onSelected?: (id: string) => void
@@ -26,6 +27,24 @@ const offset = computed(() => (currentPage.value - 1) * limit.value)
 
 const client = useBoscaClient()
 const uploader = new Uploader(client)
+
+const youtube = ref('')
+async function onAddYouTube() {
+  console.log('onAdd', youtube.value)
+  if (!youtube.value) return
+  const metadata = {
+    name: 'YouTube ' + youtube.value,
+    contentType: 'bosca/x-youtube-video',
+    languageTag: 'en',
+    attributes: {
+      'youtube.id': youtube.value,
+    },
+  } as MetadataInput
+  const id = await client.metadata.add(metadata)
+  if (props.onSelected) {
+    props.onSelected(id)
+  }
+}
 
 const { data: metadata, refresh: refreshList } = client.metadata
   .getByContentType(
@@ -43,8 +62,14 @@ async function onDrop(files: File[] | null) {
   if (!files) return
   toast({ title: 'Uploading media files, please wait...' })
   try {
-    await uploader.upload(files)
+    const ids = await uploader.upload(files)
     toast({ title: 'Media File(s) uploaded' })
+    if (props.onSelected) {
+      for (const id of ids) {
+        props.onSelected(id)
+        break
+      }
+    }
   } catch (e) {
     toast({
       title: 'Error uploading file(s)',
@@ -76,8 +101,10 @@ client.listeners.onMetadataChanged((id) => {
 </script>
 
 <template>
-  <div ref="dropZoneRef" class="h-full">
-    <div class="flex justify-between items-center">
+  <div class="h-full w-full relative">
+    <div
+      class="flex justify-between items-center absolute top-4 left-0 right-0 h-8 px-4"
+    >
       <ContentMultiContentTypeFilter :filter="filter" />
       <Pagination
         v-slot="{ page }"
@@ -113,15 +140,34 @@ client.listeners.onMetadataChanged((id) => {
         </PaginationList>
       </Pagination>
     </div>
-    <div class="grid grid-cols-6 w-full mt-6 gap-x-6 gap-y-14 pb-8">
-      <ContentMetadataImage
-        v-for="item in metadata"
-        :key="item.id"
-        :metadata="item"
-        class="w-[200px] h-[300px]"
-        aspect-ratio="portrait"
-        :on-selected="onSelected"
-      />
+    <div
+      ref="dropZoneRef"
+      class="absolute right-0 left-0 top-16 bottom-20 overflow-auto"
+    >
+      <div class="grid grid-cols-6 w-full mt-6 gap-x-6 gap-y-14">
+        <ContentMetadataImage
+          v-for="item in metadata"
+          :key="item.id"
+          :metadata="item"
+          class="w-[200px] h-[300px]"
+          aspect-ratio="portrait"
+          :on-selected="onSelected"
+        />
+      </div>
+    </div>
+    <div
+      class="flex justify-items-center absolute bottom-0 w-full h-14 border rounded-md p-2"
+    >
+      <div class="grow"></div>
+      <div class="grid place-content-center h-full text-gray-400 mr-2">
+        Drag and drop files above or provide a YouTube ID
+      </div>
+      <div class="flex gap-2">
+        <Input placeholder="Add YouTube ID" v-model="youtube" />
+        <Button @click="onAddYouTube">
+          <Icon name="i-lucide-plus" />
+        </Button>
+      </div>
     </div>
   </div>
 </template>
