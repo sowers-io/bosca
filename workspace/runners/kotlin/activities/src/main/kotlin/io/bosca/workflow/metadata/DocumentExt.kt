82e3500eb440c311a88adcf1d1235f692e0866b7
Kyle Brock <kyle@sowers.io>
2025-07-14 15:59:46 -0500

    working on splitting text for chirp

--- workspace/runners/kotlin/activities/src/main/kotlin/io/bosca/workflow/metadata/DocumentExt.kt
+++ workspace/runners/kotlin/activities/src/main/kotlin/io/bosca/workflow/metadata/DocumentExt.kt
@@ -12,6 +12,8 @@ import io.bosca.documents.ParagraphNode
 import io.bosca.documents.TextNode
 import io.bosca.graphql.fragment.Document
 import io.bosca.util.decode
+import kotlin.concurrent.atomics.AtomicLong
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 
 fun DocumentNode.filterTitle(title: Boolean): List<DocumentNode> {
     var first = true
@@ -25,57 +27,70 @@ fun DocumentNode.filterTitle(title: Boolean): List<DocumentNode> {
     }
 }
 
+@OptIn(ExperimentalAtomicApi::class)
 suspend fun Document.asText(client: Client, configuration: DocumentToTextConfiguration = DocumentToTextConfiguration()): String {
     val content = content.decode<Content>()
     val string = StringBuilder()
+    val length = AtomicLong(0)
     content?.document?.let {
         if (!configuration.includeTitle) {
             val nodes = it.filterTitle(false)
             for (node in nodes) {
-                append(node, client, string, configuration)
+                append(node, client, string, configuration, length)
             }
         } else if(configuration.includeTtsMarkup) {
             val title = it.filterTitle(true).firstOrNull()
             title?.let {
-                append(it, client, string, configuration)
-                string.append("\n")
-                string.append("[pause long]")
-                string.append("\n")
+                append(it, client, string, configuration, length)
             }
             val nodes = it.filterTitle(false)
             for (node in nodes) {
-                append(node, client, string, configuration)
+                append(node, client, string, configuration, length)
             }
         } else {
-            append(it, client, string, configuration)
+            append(it, client, string, configuration, length)
         }
-        string.append("\n\n")
+        string.append("\n")
     }
     return string.toString().trim()
 }
 
-private fun append(component: Any, key: String?, builder: StringBuilder) {
+@OptIn(ExperimentalAtomicApi::class)
+private fun append(component: Any, key: String?, builder: StringBuilder, configuration: DocumentToTextConfiguration, length: AtomicLong) {
     if (component is Map<*, *>) {
         component.entries.forEach { entry ->
-            append(entry.value!!, entry.key as String, builder)
+            append(entry.value!!, entry.key as String, builder, configuration, length)
         }
     } else if (component is List<*>) {
-        component.forEach { append(it!!, key, builder) }
+        component.forEach { append(it!!, key, builder, configuration, length) }
     } else if (component is String && key == "text") {
-        builder.append(component)
-        builder.append(" ")
+        builder.append(component, configuration, length)
+        builder.append(" ", configuration, length)
     }
 }
 
-private suspend fun append(node: DocumentNode, client: Client, builder: StringBuilder, configuration: DocumentToTextConfiguration) {
+@OptIn(ExperimentalAtomicApi::class)
+private fun StringBuilder.append(text: String, configuration: DocumentToTextConfiguration, length: AtomicLong) {
+    if (configuration.includeTtsMarkup) {
+        val len = length.addAndFetch(text.toByteArray().size.toLong())
+        if (len > 4000) {
+            length.store(0)
+            append("[split]")
+        }
+    }
+    append(text)
+}
+
+@OptIn(ExperimentalAtomicApi::class)
+private suspend fun append(node: DocumentNode, client: Client, builder: StringBuilder, configuration: DocumentToTextConfiguration, length: AtomicLong) {
     if (node is ParagraphNode) {
-        builder.append("\n")
+        builder.append("\n", configuration, length)
     }
     if (node is OrderedListNode || node is BulletListNode || node is ListItemNode) {
-        builder.append("\n")
+        builder.append("\n", configuration, length)
     }
     if (node is TextNode) {
-        builder.append(node.text.replace("’", "'"))
+        builder.append(node.text.replace("’", "'"), configuration, length)
     }
     try {
         if (node is ContainerNode) {
@@ -86,15 +101,15 @@ private suspend fun append(node: DocumentNode, client: Client, builder: StringBu
                         node.attributes.metadataId ?: error("missing bible metadata id"), null, reference
                     )
                     if (content == null) continue
-                    builder.append(content.reference.human)
+                    builder.append(content.reference.human, configuration, length)
                     if (configuration.includeTtsMarkup) {
-                        builder.append("[pause short]")
+                        builder.append(" [pause] ")
                     }
-                    builder.append("\n\n")
+                    builder.append("\n\n", configuration, length)
                     @Suppress("UNCHECKED_CAST")
-                    append(content.component as Map<String, Any>, null, builder)
+                    append(content.component as Map<String, Any>, null, builder, configuration, length)
                     if (configuration.includeTtsMarkup) {
-                        builder.append("[pause]")
+                        builder.append(" [pause] ", configuration, length)
                     }
                 }
             }
@@ -103,16 +118,21 @@ private suspend fun append(node: DocumentNode, client: Client, builder: StringBu
         println("error: failed to get bible chapter: ${e.message}")
     }
     for (child in node.content) {
-        append(child, client, builder, configuration)
+        append(child, client, builder, configuration, length)
     }
     if (node is OrderedListNode || node is BulletListNode) {
-        builder.append("\n")
+        builder.append("\n", configuration, length)
     }
     if (node is ParagraphNode) {
-        builder.append("\n\n")
-        builder.append("[pause]")
+        if (configuration.includeTtsMarkup) {
+            builder.append(" [pause] ", configuration, length)
+        }
+        builder.append("\n\n", configuration, length)
     }
     if (node is HeadingNode) {
-        builder.append("\n\n\n")
+        if (configuration.includeTtsMarkup) {
+            builder.append(" [pause] ", configuration, length)
+        }
+        builder.append("\n\n\n", configuration, length)
     }
 }
